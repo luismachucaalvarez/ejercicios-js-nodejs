f578deae29cc0adb83b8ce29c1c25953f5fe7728
Elizabeth Capuzzi <elicasma@gmail.com>
2016-06-06 10:21:27 -0300

    Se separa en un archivo el array de los pokemons

---
+++ pokemons.js
@@ -0,0 +1,7 @@
+// el archivo permite armar un array de pokemons para dar soporte a pokedexOffline.
+
+var pikachu = require('./pikachu');
+var bulbasaur = require('./bulbasaur');
+
+module.exports = [pikachu,bulbasaur];
+
