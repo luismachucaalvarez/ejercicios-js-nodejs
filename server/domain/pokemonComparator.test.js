dde9bc73357bc96982d7d4634559cb314bbbf005
Elizabeth <elicasama@gmail.com>
2016-07-04 01:36:12 -0300

    Se corrigen los test para que evaluen array de pokemons

--- server/domain/pokemonComparator.test.js
+++ server/domain/pokemonComparator.test.js
@@ -12,10 +12,10 @@ describe('PokemonComparator', function() {
 
 	describe('getTopStat()', function() {
 	  it('Recibe a pikachu y a bulbasaur, retorna pikachu', function() {
-	    expect(pokemonComparator.getTopStat(pikachu, bulbasaur)).to.equal(pikachu);
+	    expect(pokemonComparator.getTopStat([pikachu, bulbasaur])).to.equal(pikachu);
 	  });
 	  it('Recibe a bulbasaur y a pikachu, retorna pikachu', function() {
-	  	expect(pokemonComparator.getTopStat(bulbasaur, pikachu)).to.equal(pikachu);
+	  	expect(pokemonComparator.getTopStat([bulbasaur, pikachu])).to.equal(pikachu);
 	  });
 	});
 
