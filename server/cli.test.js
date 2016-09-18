52c0a75844af351439d35d79bf0d236321e52088
Rodrigo Alfonso <rodri042@gmail.com>
2016-09-18 20:07:12 -0300

    Test del cli con un criterio

--- server/cli.test.js
+++ server/cli.test.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 var expect = chai.expect;
 var CLI = require('./cli');
 var cli = new CLI();
-
+var exec = require('child_process').exec;
 
 describe('cli', function() {
 
@@ -19,4 +19,16 @@ describe('cli', function() {
 
   });
 
+  describe('Respeta la función del criterio de comparación', function() {
+    it('cuando el criterio es por mayor defensa especial', function(done) {
+      var criterio = "(function(poke) { return poke.stats.filter(function(pokeStat) { return pokeStat.stat.name === 'special-defense';  })[0].base_stat;})";
+
+      exec('node comparar.js pikachu bulbasaur charmander --criterio "' + criterio + '"', function(err, output) {
+        var lines = output.split("\n");
+        var lastLine = lines[lines.length - 2];
+        expect(lastLine).to.equal("El ganador es: bulbasaur con: 65");
+        done();
+      });
+    });
+  });
 });
