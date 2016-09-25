991a625fbed5253cdf643d66d557ca429a1cc576
Rodrigo Alfonso <rodri042@gmail.com>
2016-09-25 01:41:52 -0300

    Se realizan test automáticos para verificar la comparación por criterio

--- server/domain/pokemonComparator.test.js
+++ server/domain/pokemonComparator.test.js
@@ -6,6 +6,9 @@ var Pokemon = require('./pokemon')
 
 var pikachu = new Pokemon(require('../collections/pikachu'));
 var bulbasaur = new Pokemon(require('../collections/bulbasaur'));
+var charmander = new Pokemon(require('../collections/charmander'));
+var kakuna = new Pokemon(require('../collections/kakuna'));
+var dragonite = new Pokemon(require('../collections/dragonite'));
 
 
 describe('PokemonComparator', function() {
@@ -19,5 +22,44 @@ describe('PokemonComparator', function() {
     });
   });
 
+  describe('getByCanon', function() {
+    it('Si recibe a bulbasaur, charmander y compara por mayor defensa especial, retorna bulbasaur', function() {
+      expect(pokemonComparator.getByCanon([bulbasaur, charmander], function(poke) {
+        return poke.stats.filter(function(pokeStat) {
+          return pokeStat.stat.name === 'special-defense';
+        })[0].base_stat;
+      })).to.equal(bulbasaur);
+
+    });
+
+    it('Si recibe a bulbasaur, charmander y compara por mayor ataque, retorna charmander', function() {
+      expect(pokemonComparator.getByCanon([charmander, bulbasaur], function(poke) {
+        return poke.stats.filter(function(pokeStat) {
+         return pokeStat.stat.name === 'attack';
+        })[0].base_stat;
+      })).to.equal(charmander);
+    });
+
+    it('Si recibe a kakuna, charmander, dragonite y gana el de tipo dragón, retorna dragonite', function () {
+      expect(pokemonComparator.getByCanon([kakuna, charmander, dragonite], function (poke) {
+        return poke.types.filter(function (tipo) {
+          return tipo.type.name ==='dragon';
+        }).length;
+      })).to.equal(dragonite);
+    });
+
+    it('Si recibe a bulbasaur, pikachu y charmander y compara por la experiencia base, gana pikachu', function() {
+      expect(pokemonComparator.getByCanon([bulbasaur, pikachu, charmander], function(poke) {
+        return poke.base_experience;
+      })).to.equal(pikachu);
+    })
+
+    it('Si recive a Kakuna y dragonite y compara por mayor cantidad de habilidades, retorna dragonite', function() {
+      expect(pokemonComparator.getByCanon([kakuna, dragonite], function(pokemon) {
+        return pokemon.abilities.length
+      })).equal(dragonite);
+    })
+
+  });
 });
 
